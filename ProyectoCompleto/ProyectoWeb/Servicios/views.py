6e01e0c82fb13e3350c4df065235d82c302fad72
hectorplinio <hectorplinio@gmail.com>
2021-04-09 11:19:12 +0200

    subida

--- ProyectoCompleto/ProyectoWeb/Servicios/views.py
+++ ProyectoCompleto/ProyectoWeb/Servicios/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from Servicios.models import Servicio
 # Create your views here.
+def Servicios(request):
+    
+    Servicios=Servicio.objects.all()
+    return render(request, "Servicios/servicios.html", {"Servicios":Servicios})
