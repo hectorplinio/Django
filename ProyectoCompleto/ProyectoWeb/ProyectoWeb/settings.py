9abdc9afa8d8f5196031d8c6c1dac9b078abc581
hectorplinio <hectorplinio@gmail.com>
2021-04-12 11:57:10 +0200

    subida

--- ProyectoCompleto/ProyectoWeb/ProyectoWeb/settings.py
+++ ProyectoCompleto/ProyectoWeb/ProyectoWeb/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'Servicios',
     'blog',
     'contacto',
+    'tienda',
 ]
 
 MIDDLEWARE = [
--- ProyectoCompleto/ProyectoWeb/ProyectoWeb/urls.py
+++ ProyectoCompleto/ProyectoWeb/ProyectoWeb/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('servicios', include('Servicios.urls')),
     path('blog', include('blog.urls')),
     path('contacto/', include('contacto.urls')),
+    path('tienda/', include('tienda.urls')),
     path('', include('ProyectoWebApp.urls')),
     
 ]
